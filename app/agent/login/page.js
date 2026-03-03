f4f25454cfbcd5c8181bad9300130d47ba9852e1
RadigiHub <teamradmarketing@gmail.com>
2026-03-03 12:42:50 -0800

    Create page.js

---
+++ app/agent/login/page.js
@@ -0,0 +1,8 @@
+export default function AgentLogin() {
+  return (
+    <div style={{ padding: 30 }}>
+      <h1>Agent Login</h1>
+      <p>Coming next: OTP login connected with Supabase.</p>
+    </div>
+  );
+}
