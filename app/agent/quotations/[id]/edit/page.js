030bf789f066068d4e3a09aba95b694e69dfc73c
RadigiHub <teamradmarketing@gmail.com>
2026-03-10 07:44:01 -0700

    Update page.js

--- app/agent/quotations/[id]/edit/page.js
+++ app/agent/quotations/[id]/edit/page.js
@@ -3,7 +3,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
-import { supabase } from "../../../../../lib/supabaseClient";
+import { supabase } from "../../../../lib/supabaseClient";
 
 export default function EditQuotationPage() {
   const router = useRouter();
